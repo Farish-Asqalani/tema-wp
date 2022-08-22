4f25f51fdf51779d926be3861c7399567e4dc6a2
CL3IX <risfarishk@gmail.com>
2022-08-22 19:29:51 +0700

    responsive

--- searchForm.php
+++
@@ -1,7 +0,0 @@
-<!-- search form yang tidak jadi terpakai -->
-
- <!-- <form action="//home_url()" method="get">
-    <label for="search"></label>
-    <input type="text" name="s" id="search" value="<?php //the_search_query(); ?>">
-    <input type="submit" id="search-submit" value="Search">
-</form> --> 
